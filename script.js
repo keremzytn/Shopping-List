8520bb12fa76e4223f0da6e3fbafdfb19995028d
Mustafa Kerem Zeytun <mustafakeremzeytun1@gmail.com>
2025-04-20 22:27:49 +0300

    ikinci commit

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const shoppingList = document.querySelector(".shopping-list");
 const shoppingForm = document.querySelector(".shopping-form");
 const filterButtons = document.querySelectorAll(".filter-buttons button");
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     loadItems();
-
+    
     shoppingForm.addEventListener("submit", handleFormSubmit);
 
-    for(let button of filterButtons){
+    for(let button of filterButtons) {
         button.addEventListener("click", handleFilterSelection);
     }
 });
-
 
 function loadItems() {
     const items = [
@@ -29,50 +28,55 @@ function loadItems() {
     }
 }
 
-function addItem(input){
+function addItem(input) {
+    const id = generateId();
+    console.log(id);
     const newItem = createListItem({
-        id: generateId(),
+        id: id,
         name: input.value,
         completed: false
     });
 
     shoppingList.prepend(newItem);
-    input.value = " ";
+
+    input.value = "";
+
     updateFilteredItems();
 }
 
-function generateId(){
+function generateId() {
     return Date.now().toString();
 }
 
-function handleFormSubmit(e){
+function handleFormSubmit(e) {
     e.preventDefault();
+
     const input = document.getElementById("item_name");
 
-    if(input.value.trim().length===0){
+    if(input.value.trim().length === 0) {
         alert("yeni değer giriniz");
         return;
     }
+
     addItem(input);
 }
 
-function toggleCompleted(e){
-    const  li = e.target.parentElement;
+function toggleCompleted(e) {
+    const li = e.target.parentElement;
     li.toggleAttribute("item-completed", e.target.checked);
-    
+
     updateFilteredItems();
 }
 
-function createListItem(item){
-
-    //checkbox
+function createListItem(item) {
+    // checkbox
     const input = document.createElement("input");
     input.type = "checkbox";
     input.classList.add("form-check-input");
     input.checked = item.completed;
     input.addEventListener("change", toggleCompleted);
 
-    //item
+    // item
     const div = document.createElement("div");
     div.textContent = item.name;
     div.classList.add("item-name");
@@ -80,16 +84,15 @@ function createListItem(item){
     div.addEventListener("blur", closeEditMode);
     div.addEventListener("keydown", cancelEnter);
 
-   
-    //delete icon
+    // delete icon
     const deleteIcon = document.createElement("i");
     deleteIcon.className = "fs-3 bi bi-x text-danger delete-icon";
     deleteIcon.addEventListener("click", removeItem);
 
-    //li
+    // li
     const li = document.createElement("li");
     li.className = "border rounded p-2 mb-1";
-    li.toggleAttribute("item-completed", item.completed)
+    li.toggleAttribute("item-completed", item.completed);
 
     li.appendChild(input);
     li.appendChild(div);
@@ -98,74 +101,67 @@ function createListItem(item){
     return li;
 }
 
-function removeItem(e){
-    const  li = e.target.parentElement;
+function removeItem(e) {
+    const li = e.target.parentElement;
     shoppingList.removeChild(li);
 }
 
-function openEditMode(e){
+function openEditMode(e) {
     const li = e.target.parentElement;
-    if(li.hasAttribute("item-completed") == false){
+    if(li.hasAttribute("item-completed") == false) {
         e.target.contentEditable = true;
     }
 }
 
-function closeEditMode(e){
+function closeEditMode(e) {
     e.target.contentEditable = false;
 }
 
-function cancelEnter(e){
-    if(e.key == "Enter"){
+function cancelEnter(e) {
+    if(e.key == "Enter") {
         e.preventDefault();
         closeEditMode(e);
     }
 }
 
-function handleFilterSelection(e){
-
+function handleFilterSelection(e) {
     const filterBtn = e.target;
 
-    for(let button of filterButtons){
+    for(let button of filterButtons) {
         button.classList.add("btn-secondary");
         button.classList.remove("btn-primary");
     }
-    
+
     filterBtn.classList.add("btn-primary");
     filterBtn.classList.remove("btn-secondary");
 
     filterItems(filterBtn.getAttribute("item-filter"));
-
 }
 
-function filterItems(filterType){
-
+function filterItems(filterType) {
     const li_items = shoppingList.querySelectorAll("li");
 
-    for(let item of li_items){
+    for(let li of li_items) {
 
-        item.classList.remove("d-block");
-        item.classList.remove("d-none");
+        li.classList.remove("d-flex");
+        li.classList.remove("d-none");
 
         const item_completed = li.hasAttribute("item-completed");
 
-        if(filterType=="completed"){
-            // sadece tamamlananları göster
+        if(filterType == "completed") {
+            // tamamlananları göster
             li.classList.toggle(item_completed ? "d-flex":"d-none");
-        }
-    
-        else if(filterType=="incomplete"){
-            // sadece tamamlanmayanları göster
+        } else if(filterType == "incomplete") {
+            // tamamlanmayanları göster
             li.classList.toggle(item_completed ? "d-none":"d-flex");
-        }
-        else{
-            //hepsini göster
+        } else {
+            // hepsini göster
             li.classList.toggle("d-flex");
         }
     }
 }
 
-function updateFilteredItems(){
+function updateFilteredItems() {
     const activeFilter = document.querySelector(".btn-primary[item-filter]");
-
     filterItems(activeFilter.getAttribute("item-filter"));
 }
